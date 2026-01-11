5daf1fb441fb034c6667c3b3962e57a5fcbb1f4f
agarkovIgor <agarcov310781igor@gmail.com>
2026-01-11 18:30:39 +0300

    отправка формы страница

--- main.js
+++ main.js
@@ -94,4 +94,33 @@ document.addEventListener( 'DOMContentLoaded', function() {
   menuBackground.classList.remove("visibility");
   });
 
+  //отправка формы
+
+  const mailPath = './mail.php'
+
+document.querySelectorAll('.uniForm').forEach( (e) => {
+
+  e.addEventListener('submit', function(e) {
+
+    let th      = this,
+        params  = new FormData(this),
+        request = new XMLHttpRequest()
+
+    request.open('POST', mailPath, true)
+    request.send(params)
+
+    request.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        setTimeout(function() { th.reset() }, 1000)
+        alert('Thank you!')
+      }
+    }
+
+    e.preventDefault()
+
+  })
+
+})
+
+
 
